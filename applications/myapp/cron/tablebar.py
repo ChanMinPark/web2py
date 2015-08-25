1a77c07f1bac72e0968c4424b55b8b90dde4faa1
Park Chan-Min <walkinpcm@gmail.com>
2015-08-25 14:47:12 +0900

    Update tablebar.py

--- applications/myapp/cron/tablebar.py
+++ applications/myapp/cron/tablebar.py
@@ -5,8 +5,17 @@ from tablebar_weather import *
 from tablebar_globals import *
 #from tablebar_baseball import *
 #from gluon import current
+import logging
+import logging.handlers
 
 def main():
+    logger = logging.getLogger('mylogger')
+    fileHandler = logging.FileHandler('/usr/local/web2py/pcmtest.log')
+    logger.addHandler(fileHandler)
+    logger.setLevel(logging.DEBUG)
+    
+    logger.info('tablebar.py - main() - 1')
+    
     # Display time information
     cycle = 10
     while getTask() == "0":

573f6fdb6dc28b8bda6e16a7ad638ca6bd1f3de6
ChanMinPark <walkinpcm@gmail.com>
2015-08-28 16:35:43 +0900

    update

--- applications/myapp/modules/tablebar_LCDlogging.py
+++ applications/myapp/modules/tablebar_LCDlogging.py
@@ -4,13 +4,15 @@ from gluon import *
 import logging
 import logging.handlers
 
+logger = logging.getLogger('pcmlogger')
+formatter = logging.Formatter('[%(levelname)s|%(filename)s:%(lineno)s] %(asctime)s > %(message)s')
+fileMaxByte = 1024 * 1024 * 100 #100MB
+fileHandler = logging.handlers.RotatingFileHandler('/usr/local/web2py/logs/pcm_LCD.log', maxBytes=fileMaxByte, backupCount=1)
+fileHandler.setFormatter(formatter)
+logger.addHandler(fileHandler)
+logger.setLevel(logging.DEBUG)
+
 def LCDlog2file(line1, line2):
-    logger = logging.getLogger('pcmlogger')
-    formatter = logging.Formatter('[%(levelname)s|%(filename)s:%(lineno)s] %(asctime)s > %(message)s')
-    fileHandler = logging.FileHandler('/usr/local/web2py/logs/pcm_LCD.log')
-    fileHandler.setFormatter(formatter)
-    logger.addHandler(fileHandler)
-    logger.setLevel(logging.DEBUG)
     logger.info(line1)
     logger.info(line2)
     logger.info('================')

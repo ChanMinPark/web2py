7d4ae88bb164833a90e97381f434bffb7c589d31
Park Chan-Min <walkinpcm@gmail.com>
2015-09-19 16:16:25 +0900

    Update sendpcmlog.py

--- applications/myapp/cron/sendpcmlog.py
+++ applications/myapp/cron/sendpcmlog.py
@@ -1,10 +1,4 @@
 import requests
-import time
-
-def getTime():
-    now = time.localtime()
-    r_time = "%02d:%02d:%02d"%(now.tm_hour, now.tm_min, now.tm_sec)
-    return r_time
 
 def run_cmd(cmd):
     p = Popen(cmd, shell=True, stdout=subprocess.PIPE)
@@ -22,7 +16,7 @@ def main():
         auth=("api", "key-e5a9135df9910d5b8c14dad503d8c5e6"),
         data={"from": "Excited User <mailgun@sandbox54f98034843b4141b17c7334201d0628.mailgun.org>",
               "to": ["walkinpcm@gmail.com"],
-              "subject": "TableBar Log : %s"%(getTime()),
+              "subject": "TableBar Log",
               "text": print_tail()})
     return "Send E-mail : Okay"
 
